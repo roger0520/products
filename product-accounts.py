f678620915c694ce7a5102c2568d24e8280900b4
roger tu <zongyi0520@gmail.com>
2021-06-05 19:41:02 +0800

    add write file function

--- product-accounts.py
+++ product-accounts.py
@@ -16,3 +16,8 @@ print(products)
 for p in products:
     print(p)
     print(p[0], '的價格是', p[1])
+
+with open('products.txt', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
+
