28d0b541ca53e11169e3685d58ce2c95e8618433
roger tu <zongyi0520@gmail.com>
2021-06-06 11:37:26 +0800

    add contiune to bypass first line

--- product-accounts.py
+++ product-accounts.py
@@ -1,15 +1,15 @@
 #二維清單
-
+#讀取檔案
 products =[]
 with open('products.csv', 'r', encoding='utf-8') as f:
     for line in f:
+        if '商品,價格' in line:
+            continue #跳到下一個loop, 不會離開loop 
         name, price = line.strip().split(',')   #先進行remove \n ;在進行split功能 切割完的結果是清單
-        products.append([name, price])
-        
+        products.append([name, price])        
 print(products)
 
-
-
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -23,10 +23,12 @@ while True:
     products.append(p)   #products.append([name, price]) 
 print(products)
 
+# 印出所有購買紀錄
 for p in products:
     print(p)
     print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f: #utf-8編碼 中文字
     f.write('商品,價格\n')
     for p in products:
